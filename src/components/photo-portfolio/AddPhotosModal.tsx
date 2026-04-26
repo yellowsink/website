4a98d4efaf36e49d78c36b22787a12eda9303a5b
Hazel Atkinson <yellowsink@riseup.net>
2026-04-26 18:29:49 +0100

    uploads that are concurrent but not too concurrent

--- src/components/photo-portfolio/AddPhotosModal.tsx
+++ src/components/photo-portfolio/AddPhotosModal.tsx
@@ -1,8 +1,9 @@
-import { createEffect, onMount } from "solid-js";
+import { createEffect, createSignal, onMount } from "solid-js";
 import { addPhoto } from "./data.ts";
 
 export function AddPhotosModal(props: { isOpen: boolean; onClose: () => void; roll: number }) {
 	let modalEl: HTMLDialogElement;
+	const [displayProgress, setDisplayProgress] = createSignal(0)
 
 	onMount(() => {
 		modalEl.onclose = () => props.onClose();
@@ -16,13 +17,34 @@ export function AddPhotosModal(props: { isOpen: boolean; onClose: () => void; ro
 	return (
 		<dialog ref={modalEl}>
 			<button onclick={props.onClose}>Close</button>
+			<p>Upload progress: {displayProgress()}</p>
 			<input
 				type="file"
 				multiple
 				accept="image/png, image/jpeg, image/webp"
 				onchange={async (ev) => {
+					setDisplayProgress(0);
 					const files = ev.target.files;
-					await Promise.all([...files].map((f) => addPhoto(props.roll, f.name, f)));
+					// concurrency limiting system to not DoS michiru
+					let tickets = 2;
+					let amtDone = 0;
+
+					await new Promise<void>(bigRes => {
+						for (const f of files) {
+							(async () => {
+								// fake concurrency :)
+								while (tickets === 0) await new Promise(res => setTimeout(res, 100));
+								tickets--;
+
+								await addPhoto(props.roll, f.name, f);
+
+								tickets++;
+								amtDone++;
+								if (amtDone === files.length) bigRes();
+								setDisplayProgress(amtDone);
+							})();
+						}
+					});
 
 					props.onClose?.();
 					location.reload();
